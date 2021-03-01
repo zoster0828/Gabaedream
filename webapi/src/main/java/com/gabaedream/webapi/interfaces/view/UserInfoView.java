d031302d5062a579f1f516414edc656b79ad37a6
zoster0828 <zoster0828@gmail.com>
2021-03-01 15:39:36 +0900

    feat : user info view 추가

--- webapi/src/main/java/com/gabaedream/webapi/interfaces/view/UserInfoView.java
+++ webapi/src/main/java/com/gabaedream/webapi/interfaces/view/UserInfoView.java
@@ -3,6 +3,8 @@ package com.gabaedream.webapi.interfaces.view;
 import com.gabaedream.webapi.domain.aggregate.UserAggregate;
 
 public class UserInfoView {
+    UserAggregate userAggregate;
     public UserInfoView(UserAggregate userAggregate) {
+        this.userAggregate = userAggregate;
     }
 }
